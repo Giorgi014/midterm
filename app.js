488a38ef43ae01e5a79cc4acf292641d1f0d0dc4
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-05-13 16:40:50 +0400

    header responsive is done and some transtions are add

---
+++ app.js
@@ -0,0 +1,30 @@
+const BURGER_MENU_BUTTON = document.getElementById("burger_menu");
+const NAV_LIST = document.querySelector("ul");
+const RESPONSIVE_SEARCH_BUTTON = document.getElementById("res_search");
+const CLOSE_BUTTON = document.querySelector("i");
+const SEARCH_CONTAINER = document.getElementsByClassName("search_container");
+
+const responsiveMenu = () => {
+  const isActive = BURGER_MENU_BUTTON.classList.toggle("active");
+
+  NAV_LIST.classList.toggle("active", isActive);
+  RESPONSIVE_SEARCH_BUTTON.classList.toggle("active", isActive);
+
+  CLOSE_BUTTON.classList.toggle("fa-xmark", isActive);
+  CLOSE_BUTTON.classList.toggle("burger_menu", !isActive);
+};
+
+const closeMenu = () => {
+  BURGER_MENU_BUTTON.classList.remove("active");
+  NAV_LIST.classList.remove("active");
+  RESPONSIVE_SEARCH_BUTTON.classList.remove("active");
+  CLOSE_BUTTON.classList.remove("fa-xmark");
+};
+
+BURGER_MENU_BUTTON.addEventListener("click", () => {
+  responsiveMenu();
+  
+});
+CLOSE_BUTTON.addEventListener("click", () => {
+  closeMenu();  
+});
